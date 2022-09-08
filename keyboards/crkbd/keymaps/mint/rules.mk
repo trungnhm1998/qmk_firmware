539e697a186d1bb7aa05aba3e248b0833a5892bb
trungnhm1998 <trung.nguyen@indigames.net>
2022-09-08 21:58:37 +0700

    Implement mint corne

---
+++ keyboards/crkbd/keymaps/mint/rules.mk
@@ -0,0 +1,9 @@
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no    # Mouse keys
+
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+WPM_ENABLE      = yes
+OLED_DRIVER_ENABLE = yes
